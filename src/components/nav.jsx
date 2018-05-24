0f43220fd1be443f25e3cc8dc490c34b6d09cd34
Matt Panton <matthew.panton@gmail.com>
2018-05-24 12:44:20 -0400

    Add webpack open command to start:dev

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import queryString from 'query-string';
-import _ from 'lodash';
 
 class Nav extends Component {
   constructor(props) {
